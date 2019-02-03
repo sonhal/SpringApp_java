bedaf7efb55637fb99d4b53681a2c022e036c89c
sondre <sondre.hal@gmail.com>
2019-02-03 02:31:45 +0100

    Add property injection

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -5,11 +5,18 @@ import no.oslomet.repository.CustomerRepository;
 import no.oslomet.repository.HibernateCustomerRepositoryImpl;
 import no.oslomet.service.CustomerService;
 import no.oslomet.service.CustomerServiceImpl;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan({"no.oslomet"})
+@PropertySource("app.properties")
 public class AppConfig {
 
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
 //    @Bean(name = "customerService")
 //    public CustomerService getCustomerService(){
